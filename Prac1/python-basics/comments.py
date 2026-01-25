b1c7e64cfa1cfd8bce4bcdbb17b5445d1008ddd7
y3nl1k <asanenlik8@gmail.com>
2026-01-25 20:54:38 +0500

    Add example for commets

---
+++ Prac1/python-basics/comments.py
@@ -0,0 +1,20 @@
+#This is a comment
+print("Hello, World!")
+
+
+#Comments can be placed at the end of a line, and Python will ignore the rest of the line:
+print("Hello, World!") #This is a comment
+
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
