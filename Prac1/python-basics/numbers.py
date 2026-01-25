b99811b087bc132247b006a4eabad33e0e41db79
y3nl1k <asanenlik8@gmail.com>
2026-01-25 21:06:29 +0500

    Add example for numbers

---
+++ Prac1/python-basics/numbers.py
@@ -0,0 +1,24 @@
+#Integers:
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#Floats:
+x = 1.10
+y = 1.0
+z = -35.59
+u = 12e3
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(u))
+
+
+# complex
+z = 1j   
