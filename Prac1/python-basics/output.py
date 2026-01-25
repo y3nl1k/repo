5fcadb0290e552b120f9f21b1e2569be9d16b528
y3nl1k <asanenlik8@gmail.com>
2026-01-25 20:51:30 +0500

    Add example for output

---
+++ Prac1/python-basics/output.py
@@ -0,0 +1,26 @@
+#Each call prints text on a new line by default:
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+
+#You can use either " double quotes or ' single quotes:
+print("This will work!")
+print('This will also work!')
+
+
+#If you want to print multiple words on the same line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+
+#we don't put numbers inside double quotes:
+print(3)
+print(358)
+print(50000)
+
+
+#do math inside the print() function
+print(3 + 3)
+print(2 * 5)
+
