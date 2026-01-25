7d503075d1ce8cffee81acebd71b8ead25f3847a
y3nl1k <asanenlik8@gmail.com>
2026-01-25 20:47:16 +0500

    Add example for Python syntax

---
+++ Prac1/python-basics/syntax.py
@@ -0,0 +1,25 @@
+#indentation in Python is very important
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+#Variables in Python:
+x = 5
+y = "Hello, World!"
+
+
+#Comments in Python:
+#This is a comment.
+print("Hello, World!")
+
+
+#statements
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+
+#statements on one line by separating them with ;
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+#Best practice: Put each statement on its own line so your code is easy to understand!
