2afe13be4c1f2f12aa78ef4f65347c703f2a6c5c
y3nl1k <asanenlik8@gmail.com>
2026-02-03 22:04:45 +0500

    Add examples for IF statements

---
+++ Practice2/if_else/if_statement.py
@@ -0,0 +1,31 @@
+#If statement:
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+#Checking if a number is positive:
+
+number = 15
+if number > 0:
+  print("The number is positive")
+
+
+#Multiple statements in an if block:
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+
+#Using a boolean variable:
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+
