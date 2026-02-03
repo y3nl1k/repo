6ed3047948dbaa75086f38bd9a8d674fc11d4fc8
y3nl1k <asanenlik8@gmail.com>
2026-02-03 22:23:33 +0500

    Add examples for while loop continue

---
+++ Practice2/loops/while_continue.py
@@ -0,0 +1,18 @@
+#Continue to the next iteration if i is 3:
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+
+#Use the continue keyword in a while loop:
+
+i = 0
+while i < 9:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
