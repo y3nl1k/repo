8d7ea638c2c7541e787bc9dedf7f94158f838c44
y3nl1k <asanenlik8@gmail.com>
2026-02-03 22:22:14 +0500

    Add examples for while loop break

---
+++ Practice2/loops/while_break.py
@@ -0,0 +1,20 @@
+#Exit the loop when i is 3:
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
+#Break out of a while loop:
+
+i = 1
+while i < 9:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
