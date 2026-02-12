403a3f355e34421e7a1b133be55aa5530e9ce708
y3nl1k <asanenlik8@gmail.com>
2026-02-12 23:44:45 +0500

    Add some examples for inheritance w super function

---
+++ Practice3/inheritance/super_function.py
@@ -0,0 +1,24 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+#Python also has a super() function that will make the child class inherit all the methods and properties from its parent
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+    self.graduationyear = 2019
+
+
+x = Student("Mike", "Olsen")
+x.printname()
+print(x.graduationyear)
