612ba82eb83a9d24626a067fc502f6fd8ca67c79
y3nl1k <asanenlik8@gmail.com>
2026-02-12 22:56:41 +0500

    Add some examples for classes and objects definition

---
+++ Practice3/classes/class_definition.py
@@ -0,0 +1,13 @@
+#Create a class named MyClass, with a property named x:
+class MyClass:
+  x = 5
+#Create an object named p1, and print the value of x:
+p1 = MyClass()
+print(p1.x)
+
+#Create two objects from the MyClass class
+p2 = MyClass()
+p3 = MyClass()
+
+print(p2.x)
+print(p3.x)
